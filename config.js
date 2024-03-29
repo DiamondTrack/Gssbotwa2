5ef6463725cbe122adf9ea5ad43a98d652db8a39
DiamondTrack <74133756+DiamondTrack@users.noreply.github.com>
2024-03-29 18:53:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,8 +43,8 @@ global.mess = {
     wait: '*Processing Your request*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://aldi.my.id'
+global.linkGroup = 'https://chat.whatsapp.com/BdQKkIgiRQ8L2ZHwrbsTjv'
 global.autoswview = process.env.AUTO_STATUS_SEEN || "true",
 global.typemenu = 'v1' // 'v1' => 'v5'
 global.onlygroup = false // onlygroup
